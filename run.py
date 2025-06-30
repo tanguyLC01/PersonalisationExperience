47f434eb4bff7f342a7a721b4f4f70b9a43e70dd
Tanguy Le Cloirec <tanguy.lecloirec@gmail.com>
2025-06-30 15:47:54 +0200

    new config file and add partitioner in run file

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import logging
 from omegaconf import DictConfig
 
 from flwr_datasets.partitioner import DirichletPartitioner, IidPartitioner
-from fedper.partitioner import DirichletSkewedPartitioner
+from fedper.partitioner import DirichletSkewedPartitioner, VariablePathologicalPartitioner
 from flwr.server import ServerApp
 from fedper.utils import get_server_fn, get_client_fn
 import matplotlib.pyplot as plt
@@ -38,18 +38,40 @@ def main(cfg: DictConfig) -> None:
         partitioner = DirichletPartitioner(alpha=cfg.dataset.partitioner.alpha, num_partitions=cfg.num_clients, partition_by="label", seed=cfg.seed)
     elif cfg.dataset.partitioner.name == "dirichletskew":
         partitioner = DirichletSkewedPartitioner(num_partitions=cfg.num_clients, rich_clients=[0], alpha_rich=cfg.dataset.partitioner.alpha_rich,  alpha_poor=cfg.dataset.partitioner.alpha_poor, seed=cfg.seed)
-    fds = FederatedDataset(dataset=cfg.dataset.name, partitioners={"train": partitioner})
+    elif cfg.dataset.partitioner.name == "variable_pathological":
+        partitioner = VariablePathologicalPartitioner(
+            num_partitions=cfg.num_clients,
+            partition_by="label",
+            num_classes_per_partition=cfg.dataset.partitioner.num_classes_per_partition,
+            shuffle=True,
+            seed=cfg.seed,
+        )
+    fds = FederatedDataset(dataset=cfg.dataset.name, partitioners={"train": partitioner, "test":  VariablePathologicalPartitioner(
+            num_partitions=cfg.num_clients,
+            partition_by="label",
+            num_classes_per_partition=cfg.dataset.partitioner.num_classes_per_partition,
+            shuffle=True,
+            seed=cfg.seed,
+        )})
     
 
-    fig, _, _ = plot_label_distributions(partitioner=fds.partitioners["train"],
+    fig_train, _, _ = plot_label_distributions(partitioner=fds.partitioners["train"],
     label_name="label",
     legend=True,
     )
     
-    fig.legend()
-    fig.set_size_inches(12, 8)
+    fig_test, _, _ = plot_label_distributions(partitioner=fds.partitioners["test"],     
+                                              label_name="label",
+                                                legend=True,
+                                                )
+                                                                        
+    fig_test.legend()
+    fig_test.set_size_inches(12, 8)
+    fig_train.legend()
+    fig_train.set_size_inches(12, 8)
 
-    fig.savefig(f'{log_save_path}/samples_per_label_per_client.png', dpi=300)
+    fig_train.savefig(f'{log_save_path}/samples_per_label_per_client.png', dpi=300)
+    fig_test.savefig(f'{log_save_path}/samples_per_label_per_client_test.png', dpi=300)
     plt.close()
 
     
